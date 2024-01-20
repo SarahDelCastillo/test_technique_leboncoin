c17f40d6c69def29bd1d4247c65595ea95e55ec2
Sarah <sarah.del-castillo@outlook.com>
2024-01-20 16:15:30 +0100

    Create ListItem model

---
+++ Test Leboncoin/Models/ListItem.swift
@@ -0,0 +1,16 @@
+//
+//  ListItem.swift
+//  Test Leboncoin
+//
+//  Created by Sarah Del Castillo on 20/01/2024.
+//
+
+import Foundation
+
+struct ListItem {
+    let title: String
+    let image: URL
+    let category: String
+    let price: Double
+    let urgent: Bool
+}
